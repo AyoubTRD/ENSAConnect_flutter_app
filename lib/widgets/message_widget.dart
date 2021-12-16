fb1eb6eea984c703ea7a918dea39eff61d9bf910
Ayoub Taouarda <trd.ayoub@outlook.com>
2021-12-16 23:20:59 +0100

    .

--- lib/widgets/message_widget.dart
+++ lib/widgets/message_widget.dart
@@ -2,29 +2,96 @@ import 'package:ensa/models/message_model.dart';
 import 'package:ensa/utils/constants.dart';
 import 'package:flutter/material.dart';
 
-class MessageWidget extends StatelessWidget {
-  const MessageWidget(this._m, {Key? key}) : super(key: key);
+class MessageWidget extends StatefulWidget {
+  MessageWidget(this._m, {Key? key}) : super(key: key);
 
   final Message _m;
 
   @override
-  Widget build(BuildContext context) {
-    final bool isCurrentUser = _m.user.id == kUsers[2].id;
+  _MessageWidgetState createState() => _MessageWidgetState();
+}
 
+class _MessageWidgetState extends State<MessageWidget> {
+  late final bool isCurrentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      // TODO: Get isCurrentUser
+      isCurrentUser = widget._m.user.id == kUsers[2].id;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Align(
       alignment: isCurrentUser ? Alignment.topRight : Alignment.topLeft,
       child: Container(
-        color: isCurrentUser
-            ? Theme.of(context).accentColor
-            : Colors.grey.shade300,
         margin: EdgeInsets.only(
           left: kDefaultPadding,
           right: kDefaultPadding,
-          bottom: kDefaultPadding,
+          bottom: 10.0,
         ),
-        padding: EdgeInsets.symmetric(vertical: 3.0),
-        child: Text(_m.text),
+        child: isCurrentUser
+            ? _buildMessageText(context)
+            : Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(widget._m.user.profilePicture),
+                    backgroundColor: Theme.of(context).accentColor,
+                    radius: 26.0,
+                  ),
+                  SizedBox(
+                    width: 15.0,
+                  ),
+                  _buildMessageText(context)
+                ],
+              ),
       ),
+    );
+  }
+
+  Widget _buildMessageText(BuildContext context) {
+    const borderRadius = Radius.circular(25.0);
+
+    return Column(
+      crossAxisAlignment:
+          isCurrentUser ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+          decoration: BoxDecoration(
+            color: isCurrentUser
+                ? Theme.of(context).accentColor.withOpacity(0.11)
+                : Colors.grey.shade100,
+            borderRadius: BorderRadius.only(
+              bottomLeft: !isCurrentUser ? Radius.zero : borderRadius,
+              bottomRight: isCurrentUser ? Radius.zero : borderRadius,
+              topRight: borderRadius,
+              topLeft: borderRadius,
+            ),
+          ),
+          child: Text(
+            widget._m.text,
+            style: Theme.of(context).textTheme.bodyText2?.copyWith(
+                  color: kTextPrimary,
+                ),
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          // TODO: Format the real message time
+          '1:40 PM',
+          style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                color: kTextSecondary,
+              ),
+        ),
+      ],
     );
   }
 }
